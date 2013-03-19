03c00a6624a2b765603e5b85429ee9ec39c0e37b
Wilson Young <yyfearth@gmail.com>
2013-03-19 06:30:54 -0700

    Solve the filter make restlet return array with empty objects

--- service/src/main/java/marxo/restlet/filter/ResponseCorsFilter.java
+++ service/src/main/java/marxo/restlet/filter/ResponseCorsFilter.java
@@ -4,27 +4,24 @@ import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.MultivaluedMap;
 
 public class ResponseCorsFilter implements ContainerResponseFilter {
 
 	@Override
-	public ContainerResponse filter(ContainerRequest req, ContainerResponse contResp) {
+	public ContainerResponse filter(ContainerRequest req, ContainerResponse resp) {
 
-		ResponseBuilder resp = Response.fromResponse(contResp.getResponse());
-		resp.header("Access-Control-Allow-Origin", "*");
-		resp.header("Access-Control-Allow-Methods", "*");
+		MultivaluedMap<String, Object> headers = resp.getHttpHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "*");
 
 		String reqHead = req.getHeaderValue("Access-Control-Request-Headers");
 
 		if (null != reqHead && !reqHead.equals(null)) {
-			resp.header("Access-Control-Allow-Headers", reqHead);
+			headers.add("Access-Control-Allow-Headers", reqHead);
 		}
 
-		contResp.setResponse(resp.build());
-
-		return contResp;
+		return resp;
 	}
 
 }
