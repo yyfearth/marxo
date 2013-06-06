8208faf728b38b45ad63ba03d76f61a1b0b63617
Mason Wan <masonwan@gmail.com>
2013-06-06 13:34:51 -0700

    Remove commented code

--- service/src/main/java/marxo/dao/WorkflowDao.java
+++ service/src/main/java/marxo/dao/WorkflowDao.java
@@ -3,10 +3,4 @@ package marxo.dao;
 import marxo.bean.Workflow;
 
 public class WorkflowDao extends BasicDao<Workflow> {
-
-//	public static WorkflowDao getInstance() {
-//		return workflowDao;
-//	}
-//
-//	static WorkflowDao workflowDao = new WorkflowDao();
 }
