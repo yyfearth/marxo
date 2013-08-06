d85789b937a4daca0229a8615297432a8849a3a5
Mason Wan <masonwan@gmail.com>
2013-08-05 19:55:43 -0700

    Add Spring Data dependency

--- service/src/test/java/marxo/tool/DataGenerator.java
+++ service/src/test/java/marxo/tool/DataGenerator.java
@@ -1,34 +1,38 @@
 package marxo.tool;
 
+import marxo.bean.Workflow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This class is used to generator example data for development.
  */
 public class DataGenerator {
+	@Autowired
+	MongoTemplate mongoTemplate;
+
 	public static void main(String[] args) {
-//		MongoClient mongoClient = MongoDbConnector.getMongoClient();
-//		DB db = mongoClient.getDB("marxo");
-//		DBCollection dbCollection = db.getCollection("workflows");
-//
-//		DBCursor cursor = dbCollection.find(new BasicDBObject("IsMocked", true));
-//		while (cursor.hasNext()) {
-//			dbCollection.remove(cursor.next());
-//		}
-//
-//		ArrayList<Workflow> workflows = new ArrayList<Workflow>();
-//
-//		for (int i = 0; i < 10; i++) {
-//			Workflow w = new Workflow();
-//			w.setName("Workflow " + i);
-//			workflows.add(w);
-//		}
-//
-//		WriteResult writeResult = dbCollection.insert(TypeTool.<Workflow, DBObject>convert(workflows));
-//		mongoClient.close();
-//
-//		System.out.println("WriteResult: " + writeResult);
-//		System.out.println("Created " + writeResult.getN() + " documents to workflows.");
+		DataGenerator dataGenerator = new DataGenerator();
+		MongoTemplate mongoTemplate = dataGenerator.mongoTemplate;
+
+		mongoTemplate.remove(Query.query(Criteria.where("IsMocked").is(true)));
+
+		ArrayList<Workflow> workflows = new ArrayList<Workflow>();
+
+		for (int i = 0; i < 10; i++) {
+			Workflow w = new Workflow();
+			w.setName("Workflow " + i);
+			workflows.add(w);
+		}
+
+		mongoTemplate.insert(workflows, Workflow.class);
+
+		System.out.println("Created " + workflows.size() + " documents to Workflow collection.");
 	}
 
 	static String[] humanNames = {"Jacob", "Mason", "William", "Jayden", "Noah", "Michael", "Ethan", "Alexander", "Aiden", "Daniel", "Anthony", "Matthew", "Elijah", "Joshua", "Liam", "Andrew", "James", "David", "Benjamin", "Logan", "Christopher", "Joseph", "Jackson", "Gabriel", "Ryan", "Samuel", "John", "Nathan", "Lucas", "Christian", "Jonathan", "Caleb", "Dylan", "Landon", "Isaac", "Gavin", "Brayden", "Tyler", "Luke", "Evan", "Carter", "Nicholas", "Isaiah", "Owen", "Jack", "Jordan", "Brandon", "Wyatt", "Julian", "Aaron", "Jeremiah", "Angel", "Cameron", "Connor", "Hunter", "Adrian", "Henry", "Eli", "Justin", "Austin", "Robert", "Charles", "Thomas", "Zachary", "Jose", "Levi", "Kevin", "Sebastian", "Chase", "Ayden", "Jason", "Ian", "Blake", "Colton", "Bentley", "Dominic", "Xavier", "Oliver", "Parker", "Josiah", "Adam", "Cooper", "Brody", "Nathaniel", "Carson", "Jaxon", "Tristan", "Luis", "Juan", "Hayden", "Carlos", "Jesus", "Nolan", "Cole", "Alex", "Max", "Grayson", "Bryson", "Diego", "Jaden",};
