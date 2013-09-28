d5c1474d9e45070785277567cd61e87b21d5000b
Mason Wan <masonwan@gmail.com>
2013-09-28 07:41:47 -0700

    Remove redundant annotation

--- web-api/src/main/java/marxo/controller/BasicController.java
+++ web-api/src/main/java/marxo/controller/BasicController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
-@Controller
 public class BasicController<Entity extends BasicEntity, Dao extends BasicDao<Entity>> {
 	final Logger logger = LoggerFactory.getLogger(BasicController.class);
 }
