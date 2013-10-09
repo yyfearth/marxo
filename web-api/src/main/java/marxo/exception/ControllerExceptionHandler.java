e3c0e9782fadbce7df0ce01a2f5768bc3bc1230a
Mason Wan <MasonWan@gmail.com>
2013-10-09 15:01:38 -0700

    Move exception handler to proper package

--- web-api/src/main/java/marxo/exception/ControllerExceptionHandler.java
+++ web-api/src/main/java/marxo/exception/ControllerExceptionHandler.java
@@ -1,6 +1,6 @@
-package marxo.controller;
+package marxo.exception;
 
-import marxo.exception.*;
+import marxo.controller.ErrorJson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.ConversionNotSupportedException;
