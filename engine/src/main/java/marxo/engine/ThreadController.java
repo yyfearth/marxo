677d871dd1022e9bb77490020b01ab4a40ad2333
Mason Wan <masonwan@gmail.com>
2013-10-13 20:10:04 -0700

    Add ThreadController

---
+++ engine/src/main/java/marxo/engine/ThreadController.java
@@ -0,0 +1,33 @@
+package marxo.engine;
+
+public class ThreadController {
+	public static void main(String[] args) {
+		MessageLoop messageLoop = new MessageLoop();
+		messageLoop.run();
+	}
+}
+
+class MessageLoop implements Runnable {
+	volatile boolean isStopping = false;
+
+	boolean isStopping() {
+		return isStopping;
+	}
+
+	void setStopping(boolean stopping) {
+		isStopping = stopping;
+	}
+
+	@Override
+	public void run() {
+		while (!isStopping) {
+			System.out.println("I am working");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+}
