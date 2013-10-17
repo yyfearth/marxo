929b99712c3c0bd990c62738c74e56be0d80968b
Mason Wan <masonwan@gmail.com>
2013-10-17 03:28:09 -0700

    Change field type from array to list

--- web-api/src/main/java/marxo/exception/ErrorJson.java
+++ web-api/src/main/java/marxo/exception/ErrorJson.java
@@ -3,12 +3,16 @@ package marxo.exception;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @JsonSerialize
 public class ErrorJson {
 	@JsonProperty("errors")
-	public String[] messages;
+	public List<String> messages = new ArrayList<>();
 
 	public ErrorJson(String... messages) {
-		this.messages = messages;
+		this.messages = Arrays.asList(messages);
 	}
 }
