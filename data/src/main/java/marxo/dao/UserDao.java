65ac089541eb09c8a6bfab075e9da3551badf547
Mason Wan <masonwan@gmail.com>
2013-10-23 01:10:40 -0700

    Add UserDao

---
+++ data/src/main/java/marxo/dao/UserDao.java
@@ -0,0 +1,21 @@
+package marxo.dao;
+
+import marxo.entity.User;
+import marxo.exception.DataInconsistentException;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserDao extends BasicDao<User> {
+	public User getByEmail(String email) throws DataInconsistentException {
+		email = email.toLowerCase();
+		List<User> users = mongoTemplate.find(Query.query(Criteria.where("email").is(email)), User.class);
+
+		if (users.size() >= 2) {
+			throw new DataInconsistentException(users.size() + " users have the same email address (" + email + ").");
+		}
+
+		return (users.size() == 0) ? null : users.get(0);
+	}
+}
