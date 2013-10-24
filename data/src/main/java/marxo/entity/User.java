f47898d8544d9f0e3a2e659754b2e56a9ac4da20
Mason Wan <masonwan@gmail.com>
2013-10-24 02:30:20 -0700

    Move password validation to other places

--- data/src/main/java/marxo/entity/User.java
+++ data/src/main/java/marxo/entity/User.java
@@ -1,30 +1,35 @@
 package marxo.entity;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
-
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User extends TenantChildEntity {
 	public final static Pattern emailPattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	public String password;
-	protected String email;
+	public String email;
+	UserType userType;
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) throws InvalidArgumentException {
-		// todo: move the verification to other places.
-		Matcher matcher = emailPattern.matcher(email);
-		if (matcher.find()) {
-			this.email = email;
-		} else {
-			throw new InvalidArgumentException(new String[]{"email"});
-		}
-	}
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) throws InvalidArgumentException {
+//		// todo: move the verification to other places.
+//		Matcher matcher = emailPattern.matcher(email);
+//		if (matcher.find()) {
+//			this.email = email;
+//		} else {
+//			throw new InvalidArgumentException(new String[]{"email"});
+//		}
+//	}
 
 	public boolean checkPassword(String password) {
 		return this.password.equals(password);
+	}
+
+	@Override
+	public void fillWithDefaultValues() {
+		key = email;
+
+		super.fillWithDefaultValues();
 	}
 }
