e0d15e47f818d061ec7a79a5795fe140db3fd2c9
Mason Wan <masonwan@gmail.com>
2013-10-24 02:35:30 -0700

    Working progress

--- web-api/src/main/java/marxo/security/UserDetailsServiceImpl.java
+++ web-api/src/main/java/marxo/security/UserDetailsServiceImpl.java
@@ -1,23 +1,37 @@
 package marxo.security;
 
 import marxo.dao.UserDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import marxo.entity.User;
+import marxo.exception.DataInconsistentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.LogManager;
-
 @Service("myUserDetailService")
 public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	ApplicationContext applicationContext;
 	@Autowired
 	UserDao userDao;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-userDao.
+		User user;
+
+		try {
+			user = userDao.getByEmail(username);
+		} catch (DataInconsistentException e) {
+			e.printStackTrace();
+			return new UserDetailsImpl();
+		}
+
+		if (user == null) {
+			new UserDetailsServiceImpl();
+		}
+
+		return new UserDetailsImpl();
 	}
 }
