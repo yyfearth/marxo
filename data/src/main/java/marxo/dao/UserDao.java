1e8a60a027d5798eaa856e672887220675992e12
Mason Wan <masonwan@gmail.com>
2013-10-25 07:03:23 -0700

    Remove consistency check

--- data/src/main/java/marxo/dao/UserDao.java
+++ data/src/main/java/marxo/dao/UserDao.java
@@ -1,7 +1,6 @@
 package marxo.dao;
 
 import marxo.entity.User;
-import marxo.exception.DataInconsistentException;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
@@ -10,14 +9,7 @@ import java.util.List;
 
 @Repository
 public class UserDao extends BasicDao<User> {
-	public User getByEmail(String email) throws DataInconsistentException {
-		email = email.toLowerCase();
-		List<User> users = mongoTemplate.find(Query.query(Criteria.where("email").is(email)), User.class);
-
-		if (users.size() >= 2) {
-			throw new DataInconsistentException(users.size() + " users have the same email address (" + email + ").");
-		}
-
-		return (users.size() == 0) ? null : users.get(0);
+	public List<User> getByEmail(String email) {
+		return mongoTemplate.find(Query.query(Criteria.where("email").is(email.toLowerCase())), User.class);
 	}
 }
