5a5414fbb8001faca59212b295ee03b86d3f2cb9
Mason Wan <masonwan@gmail.com>
2013-10-29 04:44:37 -0700

    Clean up

--- web-api/src/main/java/marxo/controller/LinkController.java
+++ web-api/src/main/java/marxo/controller/LinkController.java
@@ -3,7 +3,6 @@ package marxo.controller;
 import marxo.dao.LinkDao;
 import marxo.entity.Link;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ContextResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
