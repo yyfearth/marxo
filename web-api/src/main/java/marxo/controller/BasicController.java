7a7aaba15394211eb2f82ff5e548177f630a0696
Mason Wan <masonwan@gmail.com>
2013-10-29 04:45:12 -0700

    Remove old debug code Use derived class as logger name

--- web-api/src/main/java/marxo/controller/BasicController.java
+++ web-api/src/main/java/marxo/controller/BasicController.java
@@ -5,22 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
-import javax.annotation.PostConstruct;
-
 public abstract class BasicController {
-	final Logger logger = LoggerFactory.getLogger(BasicController.class);
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	ApplicationContext applicationContext;
-
-	@PostConstruct
-	void report() {
-		Boolean isDebug = applicationContext.getBean("isDebug", Boolean.class);
-		isDebug = (isDebug == null) ? false : isDebug;
-
-		if (isDebug) {
-			logger.debug(TestController.class.getSimpleName() + " started");
-			// Prevent the JVM to prompt OutOfMemory while IntelliJ redeploys the app. (fuck dat JVM)
-//			System.gc();
-		}
-	}
 }
