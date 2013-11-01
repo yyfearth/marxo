041f6a175ce26f3ed05f15f413f3b02cf017e729
Mason Wan <masonwan@gmail.com>
2013-11-01 05:53:01 -0700

    Use RuntimeException

--- data/src/main/java/marxo/exception/ValidationException.java
+++ data/src/main/java/marxo/exception/ValidationException.java
@@ -3,7 +3,7 @@ package marxo.exception;
 import java.util.List;
 
 // review: might use RunTimeException.
-public class ValidationException extends Exception {
+public class ValidationException extends RuntimeException {
 	public final List<String> reasons;
 
 	public ValidationException(List<String> reasons) {
