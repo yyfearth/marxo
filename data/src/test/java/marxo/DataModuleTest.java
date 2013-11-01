28debefaa1017f939c9f62cb4cd149fda37e368e
Mason Wan <masonwan@gmail.com>
2013-11-01 05:52:13 -0700

    Add two more tests

--- data/src/test/java/marxo/DataModuleTest.java
+++ data/src/test/java/marxo/DataModuleTest.java
@@ -1,10 +1,13 @@
 package marxo;
 
+import marxo.dao.WorkflowDao;
 import marxo.entity.*;
 import marxo.tool.AdvancedGenerator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -26,11 +29,29 @@ public class DataModuleTest {
 		};
 
 		for (Class aClass : classes) {
-			List<BasicEntity> list = (List<BasicEntity>) mongoTemplate.findAll(aClass);
+			List<BasicEntity> list = mongoTemplate.findAll(aClass);
 			long count = list.size();
 			String message = "Collection " + aClass + " has only " + count + " record(s)";
 			assert count >= 2 : message;
 			System.out.println(message);
 		}
+	}
+
+	@Test
+	public void testPrefixCriteria() throws Exception {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath*:mongo-configuration.xml");
+		MongoTemplate mongoTemplate = (MongoTemplate) applicationContext.getBean("mongoTemplate");
+		List<Workflow> workflows = mongoTemplate.find(Query.query(new Criteria()), Workflow.class);
+		System.out.println(workflows);
+		assert workflows != null;
+	}
+
+	@Test
+	public void testAutowired() throws Exception {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mongo-configuration.xml");
+		MongoTemplate mongoTemplate = applicationContext.getBean(MongoTemplate.class);
+		WorkflowDao workflowDao = new WorkflowDao();
+		List<Workflow> workflows = workflowDao.findAll();
+		assert workflowDao.count() != 0;
 	}
 }
