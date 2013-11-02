7f93ca0b0fd8d20f91b2569a23987f8de87eb21a
Mason Wan <masonwan@gmail.com>
2013-11-02 16:54:39 -0700

    Rewrite to adapt email as identity

--- web-api/src/main/java/marxo/exception/EntityNotFoundException.java
+++ web-api/src/main/java/marxo/exception/EntityNotFoundException.java
@@ -2,12 +2,20 @@ package marxo.exception;
 
 import org.bson.types.ObjectId;
 
-public class EntityNotFoundException extends EntityException {
-	public EntityNotFoundException(ObjectId id) {
-		this(id, String.format("The entity of the given ID (%s) does not exist", id));
+public class EntityNotFoundException extends RuntimeException {
+	protected String identity;
+	protected String message;
+
+	public EntityNotFoundException(ObjectId objectId) {
+		this(objectId.toString());
 	}
 
-	protected EntityNotFoundException(ObjectId id, String message) {
-		super(id, message);
+	public EntityNotFoundException(String identity) {
+		this(identity, String.format("The entity of the given ID (%s) does not exist", identity));
+	}
+
+	protected EntityNotFoundException(String identity, String message) {
+		this.identity = identity;
+		this.message = message;
 	}
 }
