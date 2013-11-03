f49faeed745f6afeefdc3d0797ff4f446dbed0ff
Mason Wan <masonwan@gmail.com>
2013-11-03 09:34:50 -0800

    Move channel test to another class. Give up Spring MVC test

--- web-api/src/test/java/marxo/WebApiTests.java
+++ web-api/src/test/java/marxo/WebApiTests.java
@@ -8,6 +8,7 @@ import marxo.entity.Workflow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,7 +28,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.testng.Assert.assertNotNull;
 
 @WebAppConfiguration
-@ContextConfiguration("classpath*:mvc-dispatcher-servlet.xml")
+@ContextHierarchy({
+		@ContextConfiguration("classpath*:mongo-configuration.xml"),
+		@ContextConfiguration("classpath*:security.xml"),
+		@ContextConfiguration("classpath*:spring-security.xml"),
+		@ContextConfiguration("classpath*:mvc-dispatcher-servlet.xml"),
+})
 public class WebApiTests extends AbstractTestNGSpringContextTests {
 	@Autowired
 	WebApplicationContext webApplicationContext;
@@ -46,10 +52,9 @@ public class WebApiTests extends AbstractTestNGSpringContextTests {
 	public void tearDown() throws Exception {
 	}
 
-
 	@Test
 	public void getWorkflows() throws Exception {
-		MockHttpServletRequestBuilder builder = get("/api/workflows").accept(MediaType.parseMediaType("application/json;charset=UTF-8"));
+		MockHttpServletRequestBuilder builder = get("workflow").accept(MediaType.parseMediaType("application/json;charset=UTF-8"));
 		mockMvc.perform(builder)
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
@@ -57,12 +62,5 @@ public class WebApiTests extends AbstractTestNGSpringContextTests {
 		ArrayList<Workflow> workflows = objectMapper.readValue(content().toString(), ArrayList.class);
 
 		assertNotNull(workflows);
-	}
-
-	@Test
-	public void canLinkWithFacebook() throws Exception {
-		String accessToken = "CAACEdEose0cBAGdWqdr7bTlgQZBmIZANTa6cUlZAirlZBrIZAH2TzXYWdDE66bh2XnpRn1WZCZCqzYZBgpKu0axaiT7ICkH4pWfWOmdZCZBsoPlQaPvXg9bh9vYT4ihYVTUFZA4w9FiZBELmMvgNVU6xbz6Y8GA3PIb3DFulYLLR7q3hzTSximfUQJw2GvL7B0oJSpkZD";
-		FacebookClient facebookClient = new DefaultFacebookClient(accessToken);
-		User user = facebookClient.fetchObject("me", User.class);
 	}
 }
