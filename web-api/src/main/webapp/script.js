d07b4f56cbd33d0b525d0b35b478893281cfde9b
Mason Wan <masonwan@gmail.com>
2013-11-04 14:47:04 -0800

    Add credential for every link under API section

--- web-api/src/main/webapp/script.js
+++ web-api/src/main/webapp/script.js
@@ -1,7 +1,18 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", onReady);
 
 function onReady() {
     document.removeEventListener("DOMContentLoaded", onReady);
-    var anchorElement = document.querySelector('#login');
-    anchorElement.href = "http://test@example.com:test@" + location.host + "/api/workflows";
+
+    var email = encodeURIComponent('test@example.com');
+    var password = encodeURIComponent('B4driGpKjDrtdKaAoA8nUmm+D2Pl3kxoF5POX0sGSk4');
+    var uri = location.protocol + '//' + email + ':' + password + '@' + location.host + location.pathname;
+
+    console.log('uri', uri);
+
+    $('#api-list').find('a').each(function (index, element) {
+        var link = $(element).attr('href');
+        $(element).attr('href', uri + link);
+    });
 }
