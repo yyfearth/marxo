b800c297a9250d5dd031009593387e9876a1f874
Mason Wan <masonwan@gmail.com>
2013-11-05 10:46:44 -0800

    Add trace message

--- web-api/src/main/java/marxo/filter/GlobalFilter.java
+++ web-api/src/main/java/marxo/filter/GlobalFilter.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 public class GlobalFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		if (logger.isTraceEnabled()) {
+			String ip = (request.getHeader("x-real-ip") == null) ? request.getRemoteAddr() : request.getHeader("x-real-ip");
+			String port = (request.getHeader("x-real-port") == null) ? String.valueOf(request.getRemotePort()) : request.getHeader("x-real-port");
+			String queryString = (request.getQueryString() == null) ? "" : "?" + request.getQueryString();
+			logger.trace(String.format("Request from %s %s:%s for %s", request.getMethod(), ip, port, request.getRequestURL() + queryString));
+		}
 		response.addHeader("Server", "Fucking bad-ass Java-based Tomcat server (epic disasters!)");
 		filterChain.doFilter(request, response);
 	}
