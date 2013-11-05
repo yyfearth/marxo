a73d1da148788f56e574010970d06f91e36c6007
Mason Wan <masonwan@gmail.com>
2013-11-05 10:51:43 -0800

    Replace default BasicAuth filter in order to bypass authentication cache caused by cookie

---
+++ web-api/src/main/java/marxo/security/MarxoBasicAuthenticationFilter.java
@@ -0,0 +1,194 @@
+package marxo.security;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.NullRememberMeServices;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.util.Assert;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MarxoBasicAuthenticationFilter extends BasicAuthenticationFilter {
+	private AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource = new WebAuthenticationDetailsSource();
+	private AuthenticationEntryPoint authenticationEntryPoint;
+	private AuthenticationManager authenticationManager;
+	private RememberMeServices rememberMeServices = new NullRememberMeServices();
+	private String credentialsCharset = "UTF-8";
+
+	public MarxoBasicAuthenticationFilter() {
+	}
+
+//	public MarxoBasicAuthenticationFilter(AuthenticationManager authenticationManager) {
+//		super(authenticationManager);
+//		this.authenticationManager = authenticationManager;
+//	}
+
+	public MarxoBasicAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint) {
+		super(authenticationManager, authenticationEntryPoint);
+		this.authenticationManager = authenticationManager;
+		this.authenticationEntryPoint = authenticationEntryPoint;
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		Assert.notNull(this.authenticationManager, "An AuthenticationManager is required");
+
+		if (!isIgnoreFailure()) {
+			Assert.notNull(this.authenticationEntryPoint, "An AuthenticationEntryPoint is required");
+		}
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		final boolean debug = logger.isDebugEnabled();
+		final HttpServletRequest request = (HttpServletRequest) req;
+		final HttpServletResponse response = (HttpServletResponse) res;
+
+		String header = request.getHeader("Authorization");
+
+		if (header == null || !header.startsWith("Basic ")) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		try {
+			String[] tokens = extractAndDecodeHeader(header, request);
+			assert tokens.length == 2;
+
+			String username = tokens[0];
+			String password = tokens[1];
+
+			if (debug) {
+				logger.debug("Basic Authentication Authorization header found for user '" + username + "'");
+			}
+
+			if (authenticationIsRequired(username, password)) {
+				UsernamePasswordAuthenticationToken authRequest =
+						new UsernamePasswordAuthenticationToken(username, password);
+				authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+				Authentication authResult = authenticationManager.authenticate(authRequest);
+
+				if (debug) {
+					logger.debug("Authentication success: " + authResult);
+				}
+
+				SecurityContextHolder.getContext().setAuthentication(authResult);
+
+				rememberMeServices.loginSuccess(request, response, authResult);
+
+				onSuccessfulAuthentication(request, response, authResult);
+			}
+
+		} catch (AuthenticationException failed) {
+			SecurityContextHolder.clearContext();
+
+			if (debug) {
+				logger.debug("Authentication request for failed: " + failed);
+			}
+
+			rememberMeServices.loginFail(request, response);
+
+			onUnsuccessfulAuthentication(request, response, failed);
+
+			authenticationEntryPoint.commence(request, response, failed);
+
+			return;
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * Decodes the header into a username and password.
+	 *
+	 * @throws org.springframework.security.authentication.BadCredentialsException
+	 *          if the Basic header is not present or is not valid Base64
+	 */
+	private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+
+		byte[] base64Token = header.substring(6).getBytes("UTF-8");
+		byte[] decoded;
+		try {
+			decoded = Base64.decode(base64Token);
+		} catch (IllegalArgumentException e) {
+			throw new BadCredentialsException("Failed to decode basic authentication token");
+		}
+
+		String token = new String(decoded, getCredentialsCharset(request));
+
+		int delim = token.indexOf(":");
+
+		if (delim == -1) {
+			throw new BadCredentialsException("Invalid basic authentication token");
+		}
+		return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+	}
+
+	private boolean authenticationIsRequired(String username, String password) {
+		MarxoAuthentication existingAuth = (MarxoAuthentication) SecurityContextHolder.getContext().getAuthentication();
+
+		if (existingAuth == null || !existingAuth.isAuthenticated()) {
+			return true;
+		}
+
+		if (!existingAuth.getName().equals(username)) {
+			return true;
+		}
+
+		if (!password.equals(existingAuth.getCredentials())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+	                                          Authentication authResult) throws IOException {
+	}
+
+	protected void onUnsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+	                                            AuthenticationException failed) throws IOException {
+	}
+
+	protected AuthenticationEntryPoint getAuthenticationEntryPoint() {
+		return authenticationEntryPoint;
+	}
+
+	protected AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
+
+	public void setAuthenticationDetailsSource(AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
+		Assert.notNull(authenticationDetailsSource, "AuthenticationDetailsSource required");
+		this.authenticationDetailsSource = authenticationDetailsSource;
+	}
+
+	public void setRememberMeServices(RememberMeServices rememberMeServices) {
+		Assert.notNull(rememberMeServices, "rememberMeServices cannot be null");
+		this.rememberMeServices = rememberMeServices;
+	}
+
+	public void setCredentialsCharset(String credentialsCharset) {
+		Assert.hasText(credentialsCharset, "credentialsCharset cannot be null or empty");
+		this.credentialsCharset = credentialsCharset;
+	}
+
+	protected String getCredentialsCharset(HttpServletRequest httpRequest) {
+		return credentialsCharset;
+	}
+}
