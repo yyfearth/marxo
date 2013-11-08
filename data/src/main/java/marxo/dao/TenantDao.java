52f8ece725802fca7929b176f2f246960401695e
Mason Wan <masonwan@gmail.com>
2013-11-07 21:17:12 -0800

    Optimize

--- data/src/main/java/marxo/dao/TenantDao.java
+++ data/src/main/java/marxo/dao/TenantDao.java
@@ -13,13 +13,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class TenantDao extends BasicDao<Tenant> {
-	public void updateFacebookData(ObjectId tenantId, FacebookData data) {
+	public FacebookData updateFacebookData(ObjectId tenantId, FacebookData data) {
 		Query query = Query.query(Criteria.where("id").is(tenantId));
 		Update update = Update.update("facebookData", data);
 		WriteResult writeResult = mongoTemplate.updateFirst(query, update, Tenant.class);
 		if (!Strings.isNullOrEmpty(writeResult.getError())) {
 			throw new DatabaseException(String.format("[Query] %s [Update] %s [Error] %s", query.toString(), update.toString(), writeResult.getError()));
 		}
+
+		return data;
 	}
 
 	public void removeFacebookData(ObjectId tenantId) {
