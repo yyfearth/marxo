b53d6f1a6a2245857eab39bf803d9a7b4ca2bc2f
Mason Wan <masonwan@gmail.com>
2013-11-07 16:22:40 -0800

    Add parsing signed request example

--- channel/src/test/java/marxo/channel/ChannelTests.java
+++ channel/src/test/java/marxo/channel/ChannelTests.java
@@ -20,9 +20,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import sun.misc.BASE64Decoder;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 
 public class ChannelTests implements ILoggable {
 	static String appId;
@@ -37,7 +40,7 @@ public class ChannelTests implements ILoggable {
 		appId = (String) applicationContext.getBean("appId");
 		appSecret = (String) applicationContext.getBean("appSecret");
 		appToken = (String) applicationContext.getBean("appToken");
-		userToken = "CAADCM9YpGYwBACl64uziT6gUcc5kE8ZBrORcrKyMJp1VGZAqd2IZCLbCpgwBrFvUtGvDi1QyfSfIHEgyJ71TqdxXE4IGitZCvLtWVh4yJh5DYkCZBbBypn5NNJz02cZC7EUHmxwCcGYuVmk0JlAazdBi6160dgnta1zSpP2iNNOB4dp6eBZBvJtaZB9dGP4GyZBQZD";
+		userToken = "CAADCM9YpGYwBAGANsWfvdO3aEPcqWE8NM2AqeKZBjrjv3MquGBWMTDHBy8LKwd8klnZCigONqGubLv7ZAmX3dl5b2kmnx8b86ZAtK6XL63yb7BnxXd0OcYvZCjt6ZCINSd4wbdcwMzT3FHQfo91rAdWrKfSZBL47YDthTbmv1ZAbdZAZBdYaEkw6FG34gOL8P4qkkZD";
 	}
 
 	FacebookClient facebookClient = new DefaultFacebookClient(userToken);
@@ -57,6 +60,7 @@ public class ChannelTests implements ILoggable {
 			logger.info(objectMapper.writeValueAsString(user));
 		} catch (FacebookOAuthException e) {
 			logger.error(String.format("[%s] %s", e.getClass(), e.getMessage()), e);
+			assert false;
 		}
 	}
 
@@ -101,7 +105,10 @@ public class ChannelTests implements ILoggable {
 	public void testDebugToken() throws Exception {
 		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
 
-		HttpGet httpGet = new HttpGet("");
+//		HttpGet httpGet = new HttpGet(String.format("https://graph.facebook.com/debug_token?input_token=%s&access_token=%s", userToken, appToken));
+		URL url = new URL("https://graph.facebook.com/debug_token?input_token=CAADCM9YpGYwBAGANsWfvdO3aEPcqWE8NM2AqeKZBjrjv3MquGBWMTDHBy8LKwd8klnZCigONqGubLv7ZAmX3dl5b2kmnx8b86ZAtK6XL63yb7BnxXd0OcYvZCjt6ZCINSd4wbdcwMzT3FHQfo91rAdWrKfSZBL47YDthTbmv1ZAbdZAZBdYaEkw6FG34gOL8P4qkkZD&access_token=213527892138380|eZgwp-1kegwB-CI-pYi7Q2WllKw");
+		URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery());
+		HttpGet httpGet = new HttpGet(uri);
 
 		try (CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpGet)) {
 			logger.info(closeableHttpResponse.getStatusLine().toString());
@@ -118,40 +125,47 @@ public class ChannelTests implements ILoggable {
 
 	@Test
 	public void testParseSignedRequest() throws Exception {
-		String signedRequest = "";
-		String data = parseSignedRequest(signedRequest);
-		assert data != null;
+		String signedRequestString = "FxxDQB7v5bYjmhJ7_ry4VmeKpJ5bpYKR6MSk-HX8hkg.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUJNeXdEZVdtMFZFSmlPbFl6cHpid0lmR0JZcDdfT3o4MllSMGZDMTc5NXZqcTM2dXpBekIwR1J1TVhla050U1RKbDFTdFhqYm55anVEbUphd1JNUGhjVFZwUmNJV3pHNy1PSUYxM0NnQnRLYWNlSEFsbFpqcVFDT093R0o1TkxpYkVMUU9kVHB3Q0ZVSHhzNTRFNjFqdDNCZF9PQjBzbXU1SlBCb3RFRzEzUVQ0SW5KUGIzMXhaeHNRTEl4UThkbW1fbUE2RWdqWDgxMkxhbkphSmVSeWplZXRzWi1DaXR6QzlReGhOVXFyek90LTE4TG9hTlRjZmVBRnVEcWpwYnhGaVdIMjVSTjN3QWo0bFNYU25tZ1JCdkdISkl1YXFPSGVpTWFZbVVnZlp2SFpscWQ2TWRHcWc1VmJuZjdJLVRqWSIsImlzc3VlZF9hdCI6MTM4Mzc1OTc3NCwidXNlcl9pZCI6IjYzNTUwMzEwMiJ9";
+		SignedRequest signedRequest = parseSignedRequest(signedRequestString);
+		assert signedRequest != null;
+		assert signedRequest.signiture != null;
+		assert signedRequest.payload != null;
 	}
 
-	public String parseSignedRequest(String signedRequest) {
-//		list($encoded_sig, $payload) = explode('.', $signed_request, 2);
-//
-//		// decode the data
-//		$sig = base64_url_decode($encoded_sig);
-//		$data = json_decode(base64_url_decode($payload), true);
-//
-//		// confirm the signature
-//		$expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
-//		if ($sig !== $expected_sig) {
-//			error_log('Bad Signed JSON signature!');
-//			return null;
-//		}
-//
-//		return $data;
-		throw new NotImplementedException();
+	/**
+	 * <a href="https://developers.facebook.com/docs/facebook-login/using-login-with-games/#checklogin">Official docs</a>
+	 */
+	public SignedRequest parseSignedRequest(String signedRequestString) throws IOException {
+		String[] list = signedRequestString.split("\\.");
+		BASE64Decoder base64Decoder = new BASE64Decoder();
+		String sig = new String(base64Decoder.decodeBuffer(list[0]));
+		String payload = new String(base64Decoder.decodeBuffer(list[1]));
+		return new SignedRequest(sig, payload);
 	}
 
 	@Test
 	public void testSubmitPost() throws Exception {
-		FacebookType publishMessageResponse = facebookClient.publish("me/feed", FacebookType.class, Parameter.with("message", "Marxo Test"));
+		FacebookType publishMessageResponse = facebookClient.publish("me/feed", FacebookType.class, Parameter.with("message", "Marxo Test, please do some action on this post. Thanks."));
 
 		logger.info("Published message ID: " + publishMessageResponse.getId());
 	}
 
 	@Test()
 	public void testGetPost() throws Exception {
-		Post post = facebookClient.fetchObject("635503102_10152019328983103", Post.class);
+		Post post = facebookClient.fetchObject(postId, Post.class);
 		assert post != null;
+
 		logger.info(objectMapper.writeValueAsString(post));
+		assert post.getApplication().getName().equals("Marxo");
+	}
+
+	class SignedRequest {
+		public String signiture;
+		public String payload;
+
+		SignedRequest(String signiture, String payload) {
+			this.signiture = signiture;
+			this.payload = payload;
+		}
 	}
 }
