948499fc6a08869fac6971f37eff8bf2002b7632
Mason Wan <masonwan@gmail.com>
2013-11-11 20:56:43 -0800

    Add engine-related methods

--- data/src/main/java/marxo/dao/WorkflowDao.java
+++ data/src/main/java/marxo/dao/WorkflowDao.java
@@ -1,11 +1,21 @@
 package marxo.dao;
 
+import marxo.entity.ProjectStatus;
 import marxo.entity.Workflow;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class WorkflowDao extends TenantChildDao<Workflow> {
 	public WorkflowDao() {
 		super();
+	}
+
+	/**
+	 * Get next project which is waiting to be processed.
+	 */
+	public Workflow getNextProject() {
+		Criteria criteria = Criteria.where("isProject").is(true).and("status").is(ProjectStatus.IDLE);
+		return mongoTemplate.findOne(org.springframework.data.mongodb.core.query.Query.query(criteria), Workflow.class);
 	}
 }
--- engine/src/main/java/marxo/engine/EngineWorker.java
+++ engine/src/main/java/marxo/engine/EngineWorker.java
@@ -1,14 +1,16 @@
 package marxo.engine;
 
 import marxo.dao.WorkflowDao;
+import marxo.entity.Workflow;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EngineWorker implements Runnable {
 	@Autowired
 	WorkflowDao workflowDao;
 
+	// todo: make the method thread-safe, ready for multi-thread.
 	@Override
 	public void run() {
-
+		Workflow project = workflowDao.getNextProject();
 	}
 }
