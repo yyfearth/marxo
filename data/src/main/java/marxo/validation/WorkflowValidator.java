180726757f3980904c12137a99eae8adde6f49da
Mason Wan <masonwan@gmail.com>
2013-11-18 03:41:56 -0800

    Add workflow validation

--- data/src/main/java/marxo/validation/WorkflowValidator.java
+++ data/src/main/java/marxo/validation/WorkflowValidator.java
@@ -1,11 +1,54 @@
 package marxo.validation;
 
-import marxo.entity.Workflow;
+import com.google.common.collect.Maps;
+import marxo.entity.link.Link;
+import marxo.entity.node.Node;
+import marxo.entity.workflow.Workflow;
+import org.bson.types.ObjectId;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
+import org.springframework.validation.ValidationUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+todo:
+project:
+* the startNodeId should not be null
+*/
 
 // todo: find out how
-public class WorkflowValidator implements Validator {
+public class WorkflowValidator extends BasicValidator {
+	static protected MongoTemplate mongoTemplate = (MongoTemplate) new ClassPathXmlApplicationContext("classpath*:mongo-configuration.xml").getBean("mongoTemplate");
+
+	public static void wire(Workflow workflow) {
+		Query query = Query.query(Criteria.where("workflowId").is(workflow.id));
+		List<Link> links = mongoTemplate.find(query, Link.class);
+		List<Node> nodes = mongoTemplate.find(query, Node.class);
+
+		SelectIdFunction selectIdFunction = new SelectIdFunction();
+		Map<ObjectId, Link> linkMap = Maps.uniqueIndex(links, selectIdFunction);
+		Map<ObjectId, Node> nodeMap = Maps.uniqueIndex(nodes, selectIdFunction);
+
+		for (Link link : links) {
+			Node node;
+
+			node = nodeMap.get(link.previousNodeId);
+			link.previousNode = node;
+			node.tolinkIds.add(link.id);
+			node.toLinks.add(link);
+
+			node = nodeMap.get(link.nextNodeId);
+			link.nextNode = node;
+			node.fromlinkIds.add(link.id);
+			node.fromLinks.add(link);
+		}
+	}
+
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return Workflow.class.equals(clazz);
@@ -13,11 +56,17 @@ public class WorkflowValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		super.validate(target, errors);
+
 		if (!(target instanceof Workflow)) {
 			errors.reject("target.is.workflow");
 			return;
 		}
 
 		Workflow workflow = (Workflow) target;
+
+		if (workflow.isProject) {
+			ValidationUtils.rejectIfEmpty(errors, "startNodeId", "value.requires");
+		}
 	}
 }
