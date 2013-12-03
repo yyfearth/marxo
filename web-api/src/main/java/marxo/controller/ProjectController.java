5672bb28762abca2b31ebd417ba065cba6c7f32d
Mason Wan <masonwan@gmail.com>
2013-12-03 12:10:54 -0800

    Fix isProject when creating a project

--- web-api/src/main/java/marxo/controller/ProjectController.java
+++ web-api/src/main/java/marxo/controller/ProjectController.java
@@ -1,7 +1,12 @@
 package marxo.controller;
 
+import marxo.entity.workflow.Workflow;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("project{:s?}")
@@ -9,5 +14,11 @@ public class ProjectController extends WorkflowController {
 	@Override
 	protected boolean isProject() {
 		return true;
+	}
+
+	@Override
+	public Workflow create(@Valid @RequestBody Workflow entity, HttpServletResponse response) throws Exception {
+		entity.isProject = true;
+		return super.create(entity, response);
 	}
 }
