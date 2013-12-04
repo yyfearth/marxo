dfbb76934d782345b406a6511424a2a370918326
Mason Wan <masonwan@gmail.com>
2013-12-03 16:52:01 -0800

    Add new data tests

---
+++ data/src/test/java/marxo/test/DataTests.java
@@ -0,0 +1,33 @@
+package marxo.test;
+
+import com.google.common.collect.Lists;
+import marxo.entity.workflow.Notification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DataTests extends BasicDataTests {
+
+	@Test
+	public void whetherInsertDoUpdate() throws Exception {
+		Notification notification = new Notification();
+		mongoTemplate.insert(notification);
+
+		notification.setName("Hello world");
+		mongoTemplate.insert(notification);
+
+		notification = mongoTemplate.findById(notification.id, Notification.class);
+		Assert.assertNotEquals(notification.getName(), "Hello world");
+
+		notification.setName("Hello world");
+		mongoTemplate.insertAll(Lists.newArrayList(notification));
+
+		notification = mongoTemplate.findById(notification.id, Notification.class);
+		Assert.assertNotEquals(notification.getName(), "Hello world");
+
+		notification.setName("Hello world");
+		mongoTemplate.save(notification);
+
+		notification = mongoTemplate.findById(notification.id, Notification.class);
+		Assert.assertEquals(notification.getName(), "Hello world");
+	}
+}
