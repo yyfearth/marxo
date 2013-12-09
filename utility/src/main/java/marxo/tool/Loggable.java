c1718f1db9ffd2d037e3920c6a9500c746aa51a4
Mason Wan <masonwan@gmail.com>
2013-12-09 14:18:02 -0800

    Replace logback with log4j2

--- utility/src/main/java/marxo/tool/Loggable.java
+++ utility/src/main/java/marxo/tool/Loggable.java
@@ -1,8 +1,8 @@
 package marxo.tool;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public interface Loggable {
-	Logger logger = LoggerFactory.getLogger("marxo");
+	Logger logger = LogManager.getLogger("marxo");
 }
