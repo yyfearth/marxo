4f0d8835fef3c5e15d77e2cdc7a5f11296c0b91e
Mason Wan <masonwan@gmail.com>
2013-12-13 17:32:31 -0800

    Add browser and OS info to request logging

--- web-api/src/main/java/marxo/filter/GlobalFilter.java
+++ web-api/src/main/java/marxo/filter/GlobalFilter.java
@@ -1,6 +1,9 @@
 package marxo.filter;
 
 import marxo.tool.Loggable;
+import net.sf.uadetector.ReadableUserAgent;
+import net.sf.uadetector.UserAgentStringParser;
+import net.sf.uadetector.service.UADetectorServiceFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -10,13 +13,32 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class GlobalFilter extends OncePerRequestFilter implements Loggable {
+	static UserAgentStringParser userAgentStringParser = UADetectorServiceFactory.getResourceModuleParser();
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		if (Loggable.logger.isDebugEnabled()) {
 			String ip = (request.getHeader("x-real-ip") == null) ? request.getRemoteAddr() : request.getHeader("x-real-ip");
 			String port = (request.getHeader("x-real-port") == null) ? String.valueOf(request.getRemotePort()) : request.getHeader("x-real-port");
+
 			String queryString = (request.getQueryString() == null) ? "" : "?" + request.getQueryString();
-			Loggable.logger.debug(String.format("%s request from %s:%s for %s", request.getMethod(), ip, port, request.getRequestURL() + queryString));
+
+			String userAgent = request.getHeader("User-Agent");
+			String clientString;
+			if (userAgent == null) {
+				clientString = "N/A";
+			} else {
+				ReadableUserAgent agent = userAgentStringParser.parse(userAgent);
+				clientString = String.format(
+						"%s %s with %s %s",
+						agent.getOperatingSystem().getFamilyName(),
+						agent.getOperatingSystem().getVersionNumber().toVersionString(),
+						agent.getFamily(),
+						agent.getVersionNumber().getMajor()
+				);
+			}
+
+			Loggable.logger.debug(String.format("%s request from %s:%s(%s) for %s", request.getMethod(), ip, port, clientString, request.getRequestURL() + queryString));
 		}
 		response.addHeader("Server", "Fucking bad-ass Java-based Tomcat server (epic disasters!)");
 		response.addHeader("Version", "0.6");
