774a35ae5d512602978b0e18f63c6c49d6f75589
Mason Wan <masonwan@gmail.com>
2013-12-15 04:04:40 -0800

    Add restart method

--- web-api/src/main/java/marxo/controller/EngineController.java
+++ web-api/src/main/java/marxo/controller/EngineController.java
@@ -4,30 +4,33 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import marxo.engine.EngineWorker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("engine")
 public class EngineController extends BasicController {
 
 	@RequestMapping(value = "status", method = RequestMethod.GET)
-	@ResponseBody
 	public ResponseEntity<Status> getStatus() {
 		return new ResponseEntity<>(new Status(EngineWorker.isAlive()), HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "start", method = RequestMethod.PUT)
-	@ResponseBody
 	public ResponseEntity start() {
 		EngineWorker.startAsync();
 		return new ResponseEntity<>(new Response(), HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "restart", method = RequestMethod.PUT)
+	public ResponseEntity restart() {
+		EngineWorker.stop();
+		EngineWorker.startAsync();
+		return new ResponseEntity<>(new Response(), HttpStatus.OK);
+	}
+
 	@RequestMapping(value = "stop", method = RequestMethod.PUT)
-	@ResponseBody
 	public ResponseEntity stop() {
 		EngineWorker.stopAsync();
 		return new ResponseEntity<>(new Response(), HttpStatus.OK);
