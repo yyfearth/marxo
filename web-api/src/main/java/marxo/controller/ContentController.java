0e59e8da8d4368afd9f9f42d99f045da3f312a82
Mason Wan <masonwan@gmail.com>
2013-12-17 05:47:03 -0800

    Use RestController

--- web-api/src/main/java/marxo/controller/ContentController.java
+++ web-api/src/main/java/marxo/controller/ContentController.java
@@ -1,10 +1,12 @@
 package marxo.controller;
 
 import marxo.entity.action.Content;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("content{:s?}")
 public class ContentController extends EntityController<Content> {
 }
