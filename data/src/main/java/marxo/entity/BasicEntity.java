c0eb8a110df13896a1d75625b7328e38f33f89b9
Mason Wan <masonwan@gmail.com>
2013-12-18 03:56:35 -0800

    Fix toString if name is null

--- data/src/main/java/marxo/entity/BasicEntity.java
+++ data/src/main/java/marxo/entity/BasicEntity.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.base.Objects;
 import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import marxo.exception.Errors;
@@ -165,7 +166,7 @@ public abstract class BasicEntity implements MongoDbAware, Loggable {
 
 	@Override
 	public String toString() {
-		return String.format("%s:%s(%s)", getClass().getSimpleName(), name, id);
+		return String.format("%s:%s(%s)", getClass().getSimpleName(), Objects.firstNonNull(name, ""), id);
 	}
 
 	public static long count(Class<? extends BasicEntity> aClass) {
