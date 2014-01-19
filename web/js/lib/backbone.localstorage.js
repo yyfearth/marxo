6b6587d003e1f8dc8741ffec66d507c101f6b58d
Wilson Young <yyfearth@gmail.com>
2014-01-19 02:00:22 -0800

    Refine backbone localstorage adapter, and remove unneeded lib preview.js

--- web/js/lib/backbone.localstorage.js
+++ web/js/lib/backbone.localstorage.js
@@ -10,47 +10,34 @@
 
 define('lib/backbone.localstorage', ['lib/common'], function () {
 
-// A simple module to replace `Backbone.sync` with *localStorage*-based
-// persistence. Models are given GUIDS, and saved into a JSON object. Simple
-// as that.
-
-// Hold reference to Underscore.js and Backbone.js in the closure in order
-// to make things work even if they are removed from the global namespace
-
 // Generate four random hex digits.
-	/**
-	 * @return {string}
-	 */
-	function S4() {
+	function s4() {
 		return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 	}
 
-// Generate a pseudo-GUID by concatenating random hexadecimal.
-	/**
-	 * @return {string}
-	 */
+// Generate a pseudo-object-id by concatenating random hexadecimal.
 	function oid() {
-		return S4() + S4() + S4() + S4() + S4() + S4();
+		return s4() + s4() + s4() + s4() + s4() + s4();
 	}
 
 // Our Store is represented by a single JS object in *localStorage*. Create it
 // with a meaningful name, like the name you'd give a table.
 	Backbone.LocalStorage = window.Store = function (name) {
 		if (!this.localStorage) {
-			throw "Backbone.localStorage: Environment does not support localStorage."
+			throw 'Backbone.localStorage: Environment does not support localStorage.'
 		}
 		this.name = name;
 		var store = this.localStorage().getItem(this.name);
-		this.records = (store && store.split(",")) || [];
+		this.records = (store && store.split(',')) || [];
 	};
 
-	var seperator = '/'
+	var separator = '/';
 
 	_.extend(Backbone.LocalStorage.prototype, {
 
 		// Save the current state of the **Store** to *localStorage*.
 		save: function () {
-			this.localStorage().setItem(this.name, this.records.join(","));
+			this.localStorage().setItem(this.name, this.records.join(','));
 		},
 
 		// Add a model, giving it a (hopefully)-unique OID, if it doesn't already
@@ -61,7 +48,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 				model.set(model.idAttribute, model.id);
 			}
 			model.set({created_at: new Date(), updated_at: new Date()});
-			this.localStorage().setItem(this.name + seperator + model.id, JSON.stringify(model));
+			this.localStorage().setItem(this.name + separator + model.id, JSON.stringify(model.toJSON()));
 			this.records.push(model.id.toString());
 			this.save();
 			return this.find(model);
@@ -69,7 +56,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 
 		// Update a model by replacing its copy in `this.data`.
 		update: function (model) {
-			this.localStorage().setItem(this.name + seperator + model.id, JSON.stringify(model));
+			this.localStorage().setItem(this.name + separator + model.id, JSON.stringify(model.toJSON()));
 			if (!_.include(this.records, model.id.toString()))
 				this.records.push(model.id.toString());
 			model.set('updated_at', new Date());
@@ -79,7 +66,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 
 		// Retrieve a model from `this.data` by id.
 		find: function (model) {
-			return this.jsonData(this.localStorage().getItem(this.name + seperator + model.id));
+			return this.jsonData(this.localStorage().getItem(this.name + separator + model.id));
 		},
 
 		// Return the array of all models currently in storage.
@@ -87,7 +74,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 			// Lodash removed _#chain in v1.0.0-rc.1
 			return (_.chain || _)(this.records)
 				.map(function (id) {
-					return this.jsonData(this.localStorage().getItem(this.name + seperator + id));
+					return this.jsonData(this.localStorage().getItem(this.name + separator + id));
 				}, this)
 				.compact()
 				.value();
@@ -96,8 +83,8 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 		// Delete a model from `this.data`, returning it.
 		destroy: function (model) {
 			if (model.isNew())
-				return false
-			this.localStorage().removeItem(this.name + seperator + model.id);
+				return false;
+			this.localStorage().removeItem(this.name + separator + model.id);
 			this.records = _.reject(this.records, function (id) {
 				return id === model.id.toString();
 			});
@@ -109,7 +96,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 			return localStorage;
 		},
 
-		// fix for "illegal access" error on Android when JSON.parse is passed null
+		// fix for 'illegal access' error on Android when JSON.parse is passed null
 		jsonData: function (data) {
 			return data && JSON.parse(data);
 		},
@@ -117,13 +104,13 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 		// Clear localStorage for specific collection.
 		_clear: function () {
 			var local = this.localStorage(),
-				itemRe = new RegExp("^" + this.name + seperator);
+				itemRe = new RegExp('^' + this.name + separator);
 
-			// Remove id-tracking item (e.g., "foo").
+			// Remove id-tracking item (e.g., 'foo').
 			local.removeItem(this.name);
 
 			// Lodash removed _#chain in v1.0.0-rc.1
-			// Match all data items (e.g., "foo-ID") and remove.
+			// Match all data items (e.g., 'foo-ID') and remove.
 			(_.chain || _)(local).keys()
 				.filter(function (k) {
 					return itemRe.test(k);
@@ -149,7 +136,7 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 		var url = typeof model.url == 'function' ? model.url() : model.url || '';
 		if (model instanceof Backbone.Model) {
 			key = model.urlRoot || url.replace(model.id, '');
-			if (key.slice(-1) == '/')
+			if (key.slice(-1) === '/')
 				key = key.slice(0, -1);
 		} else if (model instanceof Backbone.Collection) {
 			key = url;
@@ -164,34 +151,30 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 		try {
 			console.log('Sync Req:', method, url, model.toJSON());
 			switch (method) {
-				case "read":
+				case 'read':
 					resp = model.id != undefined ? store.find(model) : store.findAll();
 					break;
-				case "create":
+				case 'create':
 					resp = store.create(model);
 					break;
-				case "update":
+				case 'update':
 					resp = store.update(model);
 					break;
-				case "delete":
+				case 'delete':
 					resp = store.destroy(model);
 					break;
 			}
 
 		} catch (error) {
 			if (error.code === DOMException.QUOTA_EXCEEDED_ERR && store._storageSize() === 0)
-				errorMessage = "Private browsing is unsupported";
+				errorMessage = 'Private browsing is unsupported';
 			else
 				errorMessage = error.message;
 		}
 
 		if (resp) {
 			if (options && options.success) {
-				if (Backbone.VERSION === "0.9.10") {
-					options.success(model, resp, options);
-				} else {
-					options.success(resp);
-				}
+				options.success(resp);
 			}
 			console.log('Sync Resp:', method, url);
 			console[model.length ? 'table' : 'dir'](model.toJSON());
@@ -200,14 +183,10 @@ define('lib/backbone.localstorage', ['lib/common'], function () {
 			}
 		} else {
 			errorMessage = errorMessage ? errorMessage
-				: "Record Not Found";
+				: 'Record Not Found';
 			console.error('Error:', errorMessage);
 			if (options && options.error)
-				if (Backbone.VERSION === "0.9.10") {
-					options.error(model, errorMessage, options);
-				} else {
-					options.error(errorMessage);
-				}
+				options.error(errorMessage);
 
 			if (syncDfd)
 				syncDfd.reject(errorMessage);
